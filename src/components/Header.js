925b91f5f30a7bc1531a86952942855599acedb5
nickcodesbetter <nicholas.kenyon.may@gmail.com>
2021-08-16 17:51:54 +0100

    Completed the basics of the exercise, finally completed something!

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import LeftMenu from "./LeftMenu"
+import Search from "./Search"
+
+function Header () {
+    return <header className="header">
+
+    <LeftMenu />
+
+    <Search />
+
+    </header>
+}
+
+
+export default Header
